6d7d8e29e0a231e5c0896ae2b09dbbad9f367f1b
JUHI NAGPURE <juhinagpure143@gmail.com>
2024-06-18 21:48:13 +0530

    added javascript code

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', function () {
+        navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+    });
+});
+
